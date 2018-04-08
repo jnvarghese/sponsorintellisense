af2c92fda9315cfbc14bb0672f232628ec693c27
jinu varghese <jobsjinu@gmail.com>
2018-04-08 18:49:04 -0400

    JV sponsorCode Changes

--- src/main/java/com/sposnor/intellisense/sponsorintellisense/mapper/StudentMapper.java
+++ src/main/java/com/sposnor/intellisense/sponsorintellisense/mapper/StudentMapper.java
@@ -67,9 +67,9 @@ public interface StudentMapper {
 			+ "WHERE S.STATUS = 0 AND FIRSTNAME LIKE #{name}  GROUP BY S.ID, STUDENTNAME, studentCode")
 	List<Student> listMatchingStudentsByName(@Param("name") String name);
 	
-	@Select("SELECT EN.ID, CONCAT(R.CODE,'-',C.CODE,'-',P.CODE,'-',SP.ID) UNIQUEID, "
-			+ "CONCAT(FIRSTNAME,' ',MIDDLEINITIAL,' ',LASTNAME ) sponsorName, studentCode, NICKNAME,  P.NAME parishName,P.CITY parishCity,C.NAME centerName, R.NAME regionName, "
-			+ "APPARTMENTNUMBER,STREET, SP.CITY sponsorCity, STATE sponsorState,POSTALCODE, SP.EMAILADDRESS sponsorEmail,  DATE_FORMAT(effectiveDate, \"%M %D %Y\") effectiveDate,  "
+	@Select("SELECT EN.ID, CONCAT(R.CODE,'-',C.CODE,'-',P.CODE,'-',SP.SPONSORCODE) UNIQUEID, "
+			+ "CONCAT(FIRSTNAME,' ',MIDDLEINITIAL,' ',LASTNAME ) sponsorName, NICKNAME,  P.NAME parishName,P.CITY parishCity,C.NAME centerName, R.NAME regionName, "
+			+ "APPARTMENTNUMBER,STREET, SP.CITY sponsorCity, STATE sponsorState,POSTALCODE, SP.EMAILADDRESS emailAddress,  DATE_FORMAT(effectiveDate, \"%M %D %Y\") effectiveDate,  "
 			+ "(CONTRIBUTIONAMOUNT + MISCAMOUNT) CONTRIBUTION, EN.CREATEDDATE FROM SPONSOR SP, PARISH P, ENROLLMENT EN , CENTER C, "
 			+ "REGION R WHERE P.ID = SP.PARISHID  AND SPONSORID = SP.ID  AND P.CENTERID = C.ID AND C.REGIONID = R.ID "
 			+ "AND EN.ID = #{id}")
